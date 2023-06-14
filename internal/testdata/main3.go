993026098c535e3d8eeabf9cd2cf190f61d040b5
Artem Yadelskyi <mymmrac@gmail.com>
2023-06-14 22:41:32 +0300

    :recycle: For loop completed

---
+++ internal/testdata/main3.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/mymmrac/routines"
+)
+
+const timeFormat = "15:04:05.999"
+
+func main() {
+	r := routines.StartRoutine()
+
+	loops := 0
+	for !r.Completed() {
+		loops++
+
+		r.Do(func() {
+			fmt.Println("Start")
+		})
+
+		r.For(0, 3, func(i int) {
+			r.Do(func() {
+				fmt.Println("Iteration", i+1, "at", time.Now().Format(timeFormat))
+			})
+			r.WaitFor(time.Second / 2)
+
+			r.For(0, 2, func(j int) {
+				r.Do(func() {
+					fmt.Println("Iteration", i+1, ":", j+1, "at", time.Now().Format(timeFormat))
+				})
+				r.WaitFor(time.Second / 2)
+			})
+		})
+
+		r.Do(func() {
+			fmt.Println("End")
+		})
+
+		r.End()
+	}
+	fmt.Println("Loops", loops)
+}
--- routine.go
+++ routine.go
@@ -10,6 +10,7 @@ type Routine struct {
 	started           bool
 	completed         bool
 	executionStack    []uintptr
+	executionSeqIndex map[string]int
 	executionSequence []string
 	executed          map[string]struct{}
 	timers            map[string]<-chan time.Time
@@ -19,6 +20,7 @@ type Routine struct {
 func NewRoutine() *Routine {
 	return &Routine{
 		executionStack:    make([]uintptr, 0),
+		executionSeqIndex: make(map[string]int),
 		executionSequence: make([]string, 0),
 		executed:          make(map[string]struct{}),
 		timers:            make(map[string]<-chan time.Time),
@@ -38,66 +40,6 @@ func (r *Routine) Started() bool {
 
 func (r *Routine) Completed() bool {
 	return r.completed
-}
-
-func (r *Routine) caller() uintptr {
-	if runtime.Callers(3, r.pc) != 1 {
-		panic(fmt.Errorf("failed to get caller"))
-	}
-
-	return r.pc[0]
-}
-
-func (r *Routine) isExecuted(callers string) (executed bool) {
-	_, executed = r.executed[callers]
-	return executed
-}
-
-func (r *Routine) isPrevExecuted(myCaller string) bool {
-	for i := len(r.executionSequence) - 1; i >= 0; i-- {
-		caller := r.executionSequence[i]
-
-		if i == len(r.executionSequence)-1 && caller == myCaller {
-			continue
-		}
-
-		if !r.isExecuted(caller) {
-			return false
-		}
-	}
-	return true
-}
-
-func (r *Routine) addExecution(caller string) {
-	for i := len(r.executionSequence) - 1; i >= 0; i-- {
-		if r.executionSequence[i] == caller {
-			return
-		}
-	}
-
-	r.executionSequence = append(r.executionSequence, caller)
-}
-
-func (r *Routine) markAsExecuted(caller string) {
-	r.executed[caller] = struct{}{}
-}
-
-func (r *Routine) executionTimer(caller string, duration time.Duration) <-chan time.Time {
-	if timer, found := r.timers[caller]; found {
-		return timer
-	}
-
-	timer := time.After(duration)
-	r.timers[caller] = timer
-	r.addExecution(caller)
-	return timer
-}
-
-func (r *Routine) pushToStack(caller uintptr) (string, func()) {
-	r.executionStack = append(r.executionStack, caller)
-	return hashCaller(r.executionStack), func() {
-		r.executionStack = r.executionStack[:len(r.executionStack)-1]
-	}
 }
 
 func (r *Routine) Start() {
@@ -145,6 +87,7 @@ func (r *Routine) Reset() {
 	r.started = false
 	r.completed = false
 	r.executionStack = make([]uintptr, 0)
+	r.executionSeqIndex = make(map[string]int)
 	r.executionSequence = make([]string, 0)
 	r.executed = make(map[string]struct{})
 	r.timers = make(map[string]<-chan time.Time)
@@ -189,7 +132,7 @@ func (r *Routine) For(start, end int, action func(i int)) {
 	if r.isExecuted(caller) {
 		return
 	}
-	if !r.isPrevExecuted(caller) {
+	if !r.isPrevExecutedTo(r.executionSequenceIndex(caller)) {
 		return
 	}
 
@@ -268,5 +211,85 @@ func (r *Routine) WaitUntilOrFor(condition func() bool, duration time.Duration) 
 		if condition() {
 			r.markAsExecuted(caller)
 		}
+	}
+}
+
+func (r *Routine) caller() uintptr {
+	if runtime.Callers(3, r.pc) != 1 {
+		panic(fmt.Errorf("failed to get caller"))
+	}
+
+	return r.pc[0]
+}
+
+func (r *Routine) isExecuted(callers string) (executed bool) {
+	_, executed = r.executed[callers]
+	return executed
+}
+
+func (r *Routine) isPrevExecuted(myCaller string) bool {
+	for _, caller := range r.executionSequence {
+		if caller == myCaller {
+			break
+		}
+
+		if !r.isExecuted(caller) {
+			return false
+		}
+	}
+	return true
+}
+
+func (r *Routine) isPrevExecutedTo(index int) bool {
+	for i, caller := range r.executionSequence {
+		if i < index {
+			break
+		}
+
+		if !r.isExecuted(caller) {
+			return false
+		}
+	}
+	return true
+}
+
+func (r *Routine) executionSequenceIndex(caller string) int {
+	i, ok := r.executionSeqIndex[caller]
+	if !ok {
+		i = len(r.executionSequence)
+		r.executionSeqIndex[caller] = i
+	}
+	return i
+}
+
+func (r *Routine) addExecution(caller string) {
+	for i := len(r.executionSequence) - 1; i >= 0; i-- {
+		if r.executionSequence[i] == caller {
+			return
+		}
+	}
+
+	r.executionSequence = append(r.executionSequence, caller)
+}
+
+func (r *Routine) markAsExecuted(caller string) {
+	r.executed[caller] = struct{}{}
+}
+
+func (r *Routine) executionTimer(caller string, duration time.Duration) <-chan time.Time {
+	if timer, found := r.timers[caller]; found {
+		return timer
+	}
+
+	timer := time.After(duration)
+	r.timers[caller] = timer
+	r.addExecution(caller)
+	return timer
+}
+
+func (r *Routine) pushToStack(caller uintptr) (string, func()) {
+	r.executionStack = append(r.executionStack, caller)
+	return hashCaller(r.executionStack), func() {
+		r.executionStack = r.executionStack[:len(r.executionStack)-1]
 	}
 }
